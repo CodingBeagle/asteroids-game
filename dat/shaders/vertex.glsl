21d623eba29bb68b30524b4129f81ad0f94ff6fc
CodingBeagle <kasper.rie@gmail.com>
2020-05-13 22:52:26 +0200

    added model and view matrix! :D

--- dat/shaders/vertex.glsl
+++ dat/shaders/vertex.glsl
@@ -13,5 +13,5 @@ uniform mat4 proj_matrix;
 
 void main(void)
 {
-    gl_Position =  proj_matrix * vec4(position, 1.0);
+    gl_Position =  proj_matrix * mv_matrix * vec4(position, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,12 @@ void APIENTRY opengl_debug_message(GLenum source, GLenum type, GLuint id, GLenum
 
 float vertices[] = {
     // Triangle One
-     0.0f, 0.0f, 0.0f,
-    100.0f, 0.0f, 0.0f,
-     100.0f, 100.0f, 0.0f
+     -0.5f,   -0.5f,    0.0f,
+      0.5f,   -0.5f,    0.0f,
+      0.5f,    0.5f,    0.0f,
+     -0.5f,   -0.5f,    0.0f,
+      0.5f,    0.5f,    0.0f,
+     -0.5f,    0.5f,    0.0f,
     };
 
 int main()
@@ -116,7 +119,15 @@ int main()
     // We have to enable it by calling glEnableVertexAttribArray.
     glEnableVertexAttribArray(0);
 
-    glm::mat4 projection = glm::ortho(0.0f, 800.0f, 600.0f, 0.0f, -1.0f, 1.0f);
+    glm::mat4 projection = glm::ortho(0.0f, 800.0f, 600.0f, 0.0f, 1.0f, -1.0f);
+
+    float camera_offset_width = 800.0f / 2.0f;
+    float camera_offset_height = 600.0f / 2.0f;
+    glm::vec2 camera_position = glm::vec2(0.0f, 0.0f);
+
+    glm::vec2 sprite_position = glm::vec2(200.0f, 100.0f);
+    glm::vec2 sprite_size = glm::vec2(50.0f, 100.0f);
+    float sprite_angle = 0.0f;
 
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
@@ -125,12 +136,35 @@ int main()
         // glUseProgram will load the compiled shaders onto the GPU hardware.
         shaderProgram.activate();
 
-        // Set Projection Matrix uniform
+        sprite_angle += 0.5f;
+
+        // VIEW MATRIX
+        // The mat4 constructor takes a single number, which is used to initialize the diagonal of a matrix
+        // With all other elements set to zero. Thus, to make an identity matrix, pass 1.0 to the constructor.
+        glm::mat4 view_matrix = glm::mat4(1.0);
+        view_matrix = glm::translate(view_matrix, glm::vec3(camera_offset_width, camera_offset_height, 0.0f));
+        view_matrix = glm::translate(view_matrix, -1.0f * glm::vec3(camera_position, 0.0f));
+
+        // MODEL MATRIX
+        // The usual order of multiplication for a model matrix is: S * R * T
+        // Because matrix multiplication happens in the order of right to left, we do it in the opposite order, thus
+        // Translate
+        // Rotate
+        // Scale
+        glm::mat4 model_matrix = glm::mat4(1.0f);
+        model_matrix = glm::translate(model_matrix, glm::vec3(sprite_position, 0.0f));
+        model_matrix = glm::rotate(model_matrix, glm::radians(sprite_angle), glm::vec3(0.0f, 0.0f, 1.0f));
+        model_matrix = glm::scale(model_matrix, glm::vec3(sprite_size.x, sprite_size.y, 0.0f));
+
+        // MODEL-VIEW MATRIX
+        glm::mat4 model_view_matrix = view_matrix * model_matrix;
+
+        // Set Projection Matrix uniform and Model-View Matrix uniform
         shaderProgram.set_uniform_value("proj_matrix", projection);
+        shaderProgram.set_uniform_value("mv_matrix", model_view_matrix);
 
         // glDrawArrays is one of several OpenGL commands which initiates the graphics pipeline processing.
-        glPointSize(30.0f);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
